46a5ca9626301738bc6f03498494e5b38dda3d1d
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-02-14 15:41:15 +0530

    5 Hello World Program

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+// var React = require('react'); // use for working with html and it includes webpack and babel
+// Because of react include modern js we can use import
+import React from 'react';
+// var ReactDOM = require('react-dom'); // use for working with DOM
+import ReactDOM from 'react-dom';
 
+// ReactDOM.render('kya dikhana hai', 'kaha dikhana hai', 'callback fun');
+// ReactDOM.render('what to show', 'where to show', 'callback fun');
+
+// Here first arguement is JSX
+ReactDOM.render(<h1>Hello World!!! I am here</h1>, document.getElementById('root'));
 
