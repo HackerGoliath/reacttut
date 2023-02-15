7de52f0935e0d3974f75ca1296fbe2e7b9547270
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-02-15 19:17:54 +0530

    23 props

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ https://picsum.photos/
 import React from 'react'; // for using JSX we must import react
 // var ReactDOM = require('react-dom'); // use for working with DOM and using render method
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 // import App from './App';
 // import "./index.css";
 
@@ -329,8 +329,43 @@ import App from './App';
 // ====================================================
 
 // Challenge-5 Simple Calculator
+// ReactDOM.render(<App />, document.getElementById("root"));
+// ==============================================
+
+// Netflix App
+import Card from "./Cards";
 ReactDOM.render(
-  <App />
-  ,
+  <>
+    <Card
+      imgsrc="https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20200813125652/dark-1.jpg"
+      sname="DARK "
+      title="A netflix Original Series"
+      link="https://www.netflix.com/in/title/80100172"
+    />
+    <Card
+      imgsrc="https://i.ytimg.com/vi/2TfYui8A5L0/maxresdefault.jpg"
+      sname="EXTRA CURRICULAR"
+      title="A netflix Original Series"
+      link="https://www.netflix.com/in/title/80990668"
+    />
+    <Card
+      imgsrc="https://www.denofgeek.com/wp-content/uploads/2017/08/stranger-things-season-3-starcourt-teaser-netflix.jpg?fit=670%2C377"
+      sname="Stranger Things"
+      title="A netflix Original Series"
+      link="https://www.netflix.com/in/title/80057281"
+    />
+    <Card
+      imgsrc="https://i.ytimg.com/vi/ulOOON_KYHs/maxresdefault.jpg"
+      sname="1899"
+      title="A netflix Original Series"
+      link="https://www.netflix.com/in/title/80214497"
+    />
+    <Card
+      imgsrc="https://i.ytimg.com/vi/eNQIlMU8Zkg/maxresdefault.jpg"
+      sname="ALL OF US ARE DEAD"
+      title="A netflix Original Series"
+      link="https://www.netflix.com/in/title/81237994"
+    />
+  </>,
   document.getElementById("root")
 );
