2a311ed9dd724550b94b39ba00fbac84c0edb6ca
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-01 17:00:14 +0530

    6 Nested Routing

--- src/React Router DOM/v6/App.jsx
+++ src/React Router DOM/v6/App.jsx
@@ -17,10 +17,24 @@ const App = () => {
                     <Route path='about' element={<div>About Page</div>} />
                     <Route path='contact' element={<div>Contact Page</div>} /> */}
 
-                    <Route path='/' element={<Home />} />
+                    {/* <Route path='/' element={<Home />} />
                     <Route path='about' element={<About />} />
                     <Route path='contact' element={<Contact />} />
-                    <Route path='*' element={<Error />} />
+                    <Route path='*' element={<Error />} /> */}
+
+                    {/* Nested Routing */}
+                    {/* <Route path='/' element={<Home />} >
+                        <Route path='about' element={<About />} />
+                        <Route path='contact' element={<Contact />} />
+                        <Route path='*' element={<Error />} />
+                    </Route> */}
+
+                    {/* Shared Layout */}
+                    <Route path='/' element={<Home />} >
+                        <Route path='about' element={<About />} />
+                        <Route path='contact' element={<Contact />} />
+                        <Route path='*' element={<Error />} />
+                    </Route>
                 </Routes>
             </BrowserRouter>
         </>
