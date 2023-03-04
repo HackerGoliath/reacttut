e1a92dc8e882a2b1d4e9c262c971e573fb60a06e
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-04 16:47:07 +0530

    4 Work with arrays

--- src/Hooks/new/components/UseStateArray.jsx
+++ src/Hooks/new/components/UseStateArray.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UseStateArray = () => {
+    const bioData = [
+        {
+            id: 1, name: "Deepak", age: 26
+        },
+        {
+            id: 2, name: "Babulal", age: 27
+        },
+        {
+            id: 3, name: "Bablu", age: 21
+        },
+    ]
+    // console.log(bioData);
+    const [myArray, setMyArray] = useState(bioData);
+    const clearArray = () => {
+        setMyArray([]);
+    }
     return (
-        <div>UseStateArray</div>
+        <div>
+            {
+                myArray.map((currElem, ind) => <h1 key={ind} className="h1Style shadow-lg m-3">Name : {currElem.name} and age : {currElem.age}</h1>
+                    // console.log(currElem);
+                )
+            }
+            <button className='btn btn-info shadow-lg px-5 my-2' onClick={clearArray}>Clear</button>
+        </div>
     )
 }
 
