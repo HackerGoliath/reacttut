20a94b71beae537cd886ae15d6e3656d8f026511
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2023-03-12 14:19:50 +0530

    8 Delete a single user

--- src/Redux/ReduxToolkit/store/slices/UserSlice.jsx
+++ src/Redux/ReduxToolkit/store/slices/UserSlice.jsx
@@ -6,9 +6,22 @@ const userSlice = createSlice({
     reducers: {
         addUser(state, action) {
             state.push(action.payload)
-            console.log(action.payload);
+            // console.log(action.payload);
         },
-        removeUser(state, action) { },
+        removeUser(state, action) {
+            // console.log("hi", action.payload);
+
+            // Deletes last element
+            state.pop(action.payload)
+
+            // Delete clicked element(2nd arg is the no of element you want to delete)
+            state.splice(action.payload, 1)
+
+            // Other Method
+            // let userIndexNum = state.indexOf(action.payload)
+            // state.splice(userIndexNum,1f)
+        },
+
         deleteUser(state, action) { }
     }
 });
@@ -16,4 +29,4 @@ const userSlice = createSlice({
 // console.log(userSlice);
 // console.log(userSlice.actions);
 export default userSlice.reducer;
-export const { addUser } = userSlice.actions;
+export const { addUser, removeUser } = userSlice.actions;
